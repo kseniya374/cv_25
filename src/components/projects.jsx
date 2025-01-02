f19339046faf1ba23772277a88e6b41717015bd2
kseniya374 <kseniya_prankevich05@mail.ru>
2025-01-03 00:17:55 +0300

    Add files via upload

---
+++ src/components/projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Examples of projects</h2>
+      <ul>
+        <li>
+          <h3>Todolist</h3>
+          <a href="https://github.com/kseniya374/cv">link to code</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Projects;
