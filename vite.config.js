04caf512bfac74948f4954795e7548d853fc485f
kseniya374 <kseniya_prankevich05@mail.ru>
2025-01-03 00:27:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://kseniya374.github.io/cv_25'
+  base:'https://kseniya374.github.io/cv_25/'
 })
